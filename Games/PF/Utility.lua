2ec0b8898abd8dd08ead70a1d2e073e60e19ee3b
xurel <151750472+xurel7@users.noreply.github.com>
2024-11-13 20:06:44 -0500

    Update Utility.lua

--- Games/PF/Utility.lua
+++ Games/PF/Utility.lua
@@ -1,1 +1,39 @@
+local module = {}
 
+local GetService = cloneref or function(...) return ... end
+
+local Players = GetService(game:GetService("Players")) :: Players
+local Teams = GetService(game:GetService("Teams")) :: Teams
+
+local LocalPlayer = Players.LocalPlayer
+
+local Bodyparts = {
+  ["Head"] = "rbxassetid://6179256256",
+  ["Torso"] = "rbxassetid://4049240078"
+}
+
+function module.plrAllied(Character: Model): boolean
+  if not Character then
+    return true
+  end
+  local HF = Character:FindFirstChildOfClass("Folder")
+  local H = HF and HF:FindFirstChildOfClass("MeshPart")
+  if not HF then
+    return true
+  end
+  if H.BrickColor == BrickColor.new("Black") then
+    return Teams.Phantoms == LocalPlayer.Team
+  end
+  return Teams.Ghosts == LocalPlayer.Team
+end
+
+function module.getBodypart(Character: Model, Bodypart: string): BasePart
+  local Target = Bodyparts[Bodypart] or Bodyparts["Head"]
+  for _, Part in ipairs(Character:GetChildren()) do
+    if Part:IsA("SpecialMesh") and string.find(Part.MeshId, Target) then
+      return Part
+    end
+  end
+end
+
+return module
