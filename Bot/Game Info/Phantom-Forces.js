61dc401593e82a3bcb56ca711f429c0512632267
xurel <151750472+xurel7@users.noreply.github.com>
2024-11-19 16:27:45 -0500

    Update and rename PF.js to Phantom-Forces.js

---
+++ Bot/Game Info/Phantom-Forces.js
@@ -0,0 +1,22 @@
+{
+    "status": "online",
+    "warnings": "Do not exploit on your main account. Phantom Forces has a good moderation team.",
+    "features": [
+        {
+            "name": "Aimbot",
+            "description": "Moves your mouse/camera in the direction of the target."
+        },
+        {
+            "name": "Silent Aim",
+            "description": "Moves your bullets in the direction of the target."
+        },
+        {
+            "name": "ESP",
+            "description": "Allows you to see enemies positions through walls."
+        },
+        {
+            "name": "Chat Bypass",
+            "description": "Allows you to say anything in the chat. (CERTAIN WORDS WONT WORK)"
+        }
+    ]
+}
